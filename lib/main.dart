07922247e5f1aff58fdabe18ee0e731c4fc4ca2d
Sidd <psiddhu001@gmail.com>
2023-07-23 18:59:51 -0400

    Added bit of widget style

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,29 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(
+  runApp(MaterialApp(
     home: Scaffold(
-      body: Center(
-        child: Text('Hello World!'),
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color.fromRGBO(48, 5, 91, 1),
+              Color.fromRGBO(79, 9, 114, 1),
+            ],
+            begin: Alignment.bottomCenter,
+            end: Alignment.topCenter,
+          ),
+        ),
+        child: const Center(
+          child: Text(
+            'First App!',
+            style: TextStyle(
+              fontSize: 32,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
+          ),
+        ),
       ),
     ),
   ));
